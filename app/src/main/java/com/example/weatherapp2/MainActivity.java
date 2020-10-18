eb9ec4efa9e307e85878b29ff00a5b088636b1e7
marco poscente <poscentemarco@hotmail.it>
2020-10-18 17:48:57 +0200

    dalli

--- app/src/main/java/com/example/weatherapp2/MainActivity.java
+++ app/src/main/java/com/example/weatherapp2/MainActivity.java
@@ -31,7 +31,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DecimalFormat;
-//okok
+
 public class MainActivity extends AppCompatActivity {
 
     AutoCompleteTextView cityName;
