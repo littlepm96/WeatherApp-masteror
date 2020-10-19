0a031d5e6ac48595cd4cbaaad1e514c44f8c68e2
TeraFlipFlop <48834256+TeraFlipFlop@users.noreply.github.com>
2020-10-19 20:31:01 +0200

    smol.json è di test ez.json sono le città italiane(forse me ne so persa qualcuna) cityy.json sono tutte

--- app/src/main/java/com/example/weatherapp2/Activity2.java
+++ app/src/main/java/com/example/weatherapp2/Activity2.java
@@ -4,15 +4,94 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.util.ArrayList;
 
 public class Activity2 extends AppCompatActivity {
+    public  String  loadFromRaw(){
 
+        InputStream is = getResources().openRawResource(R.raw.city);
+
+
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = null;
+            try {
+                reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+                int n;
+                while ((n = reader.read(buffer)) != -1) {
+                    writer.write(buffer, 0, n);
+                }
+            } finally {
+                is.close();
+            }} catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String jsonString = writer.toString();
+
+
+
+
+
+        return  jsonString;
+    }
+
+
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getResources().openRawResource(R.raw.ez);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        ArrayList<String> city=new ArrayList<>();
+        try {
+            JSONArray array = new JSONArray(loadJSONFromAsset());
+
+            for (int i=0;i< array.length();i++){
+            JSONObject cit = array.getJSONObject(i);
+            if(!(cit.get("country").toString().equals("IT"))) {}
+            else city.add((String) cit.get("name")); }
+
+
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
 
         ArrayList<String> cities =new ArrayList<>();
         String a=new String("rrr");
@@ -29,7 +108,7 @@ public class Activity2 extends AppCompatActivity {
 
         RecyclerView recyclerView =findViewById(R.id.view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new adapter(cities));
+        recyclerView.setAdapter(new adapter(city));
     };
 
 
